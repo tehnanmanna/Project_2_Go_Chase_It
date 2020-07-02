64e7f4c162ff06e661838ca523c20c74cd15411c
tehnanmanna <tahnanmanna@gmail.com>
2020-07-02 07:04:17 -0700

    Adding

--- build/Untitled Folder/ball_chaser/CMakeFiles/drive_bot.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/drive_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/Untitled Folder/ball_chaser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tehnanmanna/Project_2/src/Untitled Folder/ball_chaser
-# Build directory: /home/tehnanmanna/Project_2/build/Untitled Folder/ball_chaser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
